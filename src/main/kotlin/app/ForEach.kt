44598c2fa1bd0d6cb6b0ea0aebd0f4a44757bc20
Daud Hidayat R <daud28ramadhan@gmail.com>
2023-10-04 11:54:15 +0700

    belajar Collection Operations
    Signed-off-by: Daud Hidayat R <daud28ramadhan@gmail.com>

---
+++ src/main/kotlin/app/ForEach.kt
@@ -0,0 +1,7 @@
+package app
+
+fun main(){
+    listOf("Daud", "Hidayat", "Ramadhan").forEach { println(it) }
+    setOf("Daud", "Hidayat", "Ramadhan").forEach { println(it) }
+    mutableListOf("Daud", "Hidayat", "Ramadhan").forEach { println(it) }
+}
